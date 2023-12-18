adfb137fd134a47a0028403e578283d2154bb4d2
ajsams <ajsams@me.com>
2023-12-18 17:31:19 -0500

    Update setup.py

--- dsutils/setup.py
+++ dsutils/setup.py
@@ -2,8 +2,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="dsutils",
-    packages=find_packages(),
+    packages=find_packages(include=["dsutils"]),
     version="0.1.0",
     description="Personal data science utilities library.",
     author="Aaron J. Sams",
+    install_requires=[],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest==7.4.3"],
+    test_suite="tests",
 )
